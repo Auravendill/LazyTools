33005d0800134f49531306d53ab06bf98fd28746
Auravendill <auravendill@gmx.de>
2023-04-10 21:44:41 +0200

    added prints

--- themes.py
+++ themes.py
@@ -3,6 +3,8 @@ from os import path
 from os import mkdir
 from os import system as cl
 
+print("Starting program...")
+
 cd(path.expanduser("~"))
 
 if not path.exists("Git"):
@@ -14,6 +16,8 @@ if not path.exists("Themes"):
     print("Themes folder missing. Generate new one")
     mkdir("Themes")
 cd("Themes")
+
+print("#"*10+"Orchis"+"#"*10)
 
 if not path.exists("Orchis-theme"):
     print("Orchis-theme folder missing. Generate new one")
@@ -31,6 +35,8 @@ cl("./install.sh --tweaks primary solid")
 
 cd("..")
 
+print("#"*10+"Cursor"+"#"*10)
+
 if not path.exists("Vimix-cursors"):
     print("Vimix-cursors folder missing. Generate new one")
     cl("git clone https://github.com/vinceliuice/Vimix-cursors.git")
@@ -38,6 +44,8 @@ cd("Vimix-cursors")
 cl("sudo ./install.sh")
 
 cd("..")
+
+print("#"*10+"Icons"+"#"*10)
 
 if not path.exists("Reversal-icon-theme"):
     print("Reversal-icon-theme folder missing. Generate new one")
