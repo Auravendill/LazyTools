028964945ed270f6deb8db5b5f7a98f63077d04f
Auravendill <auravendill@gmx.de>
2023-04-10 21:38:42 +0200

    simple script to build/install all my preferred themes

---
+++ themes.py
@@ -0,0 +1,46 @@
+from os import chdir as cd
+from os import path
+from os import mkdir
+from os import system as cl
+
+cd(path.expanduser("~"))
+
+if not path.exists("Git"):
+    print("Git folder missing. Generate new one")
+    mkdir("Git")
+cd("Git")
+
+if not path.exists("Themes"):
+    print("Themes folder missing. Generate new one")
+    mkdir("Themes")
+cd("Themes")
+
+if not path.exists("Orchis-theme"):
+    print("Orchis-theme folder missing. Generate new one")
+    cl("git clone https://github.com/vinceliuice/Orchis-theme.git")
+cd("Orchis-theme")
+cl("sudo ./install.sh --tweaks primary solid")
+
+cd ("..")
+
+if not path.exists("Orchis-kde"):
+    print("Orchis-kde folder missing. Generate new one")
+    cl("git clone https://github.com/vinceliuice/Orchis-kde.git")
+cd("Orchis-kde")
+cl("./install.sh --tweaks primary solid")
+
+cd("..")
+
+if not path.exists("Vimix-cursors"):
+    print("Vimix-cursors folder missing. Generate new one")
+    cl("git clone https://github.com/vinceliuice/Vimix-cursors.git")
+cd("Vimix-cursors")
+cl("sudo ./install.sh")
+
+cd("..")
+
+if not path.exists("Reversal-icon-theme"):
+    print("Reversal-icon-theme folder missing. Generate new one")
+    cl("git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git")
+cd("Reversal-icon-theme")
+cl("sudo ./install.sh -a")
