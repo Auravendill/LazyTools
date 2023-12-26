bf5f850167b4ea4dfa8738ef7e03f3aa248b4e61
Auravendill <Daniel-Wolters@gmx.de>
2023-12-26 18:22:25 +0100

    sort

--- sort.sh
+++ sort.sh
@@ -4,11 +4,13 @@ mkdir exe
 mkdir img
 mkdir pdf
 mkdir vid
+mkdir gif
 
 find . -type f -name "*.exe" -print0 | xargs -0 mv -t ./exe
 
 find . -type f -name "*.png" -print0 | xargs -0 mv -t ./img
 find . -type f -name "*.jpg" -print0 | xargs -0 mv -t ./img
+find . -type f -name "*.heic" -print0 | xargs -0 mv -t ./img
 
 find . -type f -name "*.pdf" -print0 | xargs -0 mv -t ./pdf
 
@@ -20,5 +22,7 @@ find . -type f -name "*.avi" -print0 | xargs -0 mv -t ./vid
 find . -type f -name "*.wmv" -print0 | xargs -0 mv -t ./vid
 find . -type f -name "*.ts"  -print0 | xargs -0 mv -t ./vid
 find . -type f -name "*.webm" -print0 | xargs -0 mv -t ./vid
+
+find . -type f -name "*.gif" -print0 | xargs -0 mv -t ./gif
 
 find . -type d -empty -delete -print
