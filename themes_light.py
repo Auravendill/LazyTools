d202ae7a8afa1a710defcaeccf2e4064b7d5dc34
Auravendill <Daniel-Wolters@gmx.de>
2024-10-11 19:35:47 +0200

    a light version for machines without GUI

---
+++ themes_light.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from os import chdir as cd
+from os import path
+from os import mkdir
+from os import system as cl
+
+print("Starting program...")
+
+cd(path.expanduser("~"))
+
+if not path.exists("Git"):
+    print("Git folder missing. Generate new one")
+    mkdir("Git")
+cd("Git")
+
+#Nothing for now here...
+
+cd(path.expanduser("~"))
+cl("cargo install topgrade cargo-update cargo-cache bottom")
